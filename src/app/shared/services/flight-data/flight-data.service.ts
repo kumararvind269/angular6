b6bcc29f7be3d1dc7ffe1493fccdbd683df536a8
kumararvind269 <36441117+kumararvind269@users.noreply.github.com>
2019-01-04 07:27:27 +0530

    Add files via upload

--- src/app/shared/services/flight-data/flight-data.service.ts
+++ src/app/shared/services/flight-data/flight-data.service.ts
@@ -1,1 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Observable, ReplaySubject, Subscriber } from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
+@Injectable({
+  providedIn: 'root'
+})
+export class FlightDataService {
+
+  constructor(private readonly httpClient: HttpClient) { }
+
+  public getFlights(): Observable<any> {
+    const apiUrl = 'http://localhost:4200/flights';
+    return new Observable((subsciber: Subscriber<any>) => {
+        this.httpClient.get<any>(apiUrl).subscribe(result => {
+            subsciber.next(result);
+            subsciber.complete();
+        }, (error) => {
+            subsciber.error();
+            subsciber.complete();
+        });
+
+    });
+  }
+
+  public getCities(): Observable<any> {
+    const apiUrl = 'http://localhost:4200/cities';
+    return new Observable((subsciber: Subscriber<any>) => {
+        this.httpClient.get<any>(apiUrl).subscribe(result => {
+            subsciber.next(result);
+            subsciber.complete();
+        }, (error) => {
+            subsciber.error();
+            subsciber.complete();
+        });
+
+    });
+  }
+}
