a71e6f3af6fd4bed86d1af8f92c7db3516aa7b58
kumararvind269 <36441117+kumararvind269@users.noreply.github.com>
2019-01-04 07:25:56 +0530

    Add files via upload

--- src/app/shared/models/filter.ts
+++ src/app/shared/models/filter.ts
@@ -1,1 +1,11 @@
-
+import * as moment from 'moment';
+export class Filter {
+    [key: string]: any;
+    constructor(
+    public sourceCity: string,
+    public destinationCity: string,
+    public departureDate: moment.Moment,
+    public passengers: number,
+    public returnDate?: moment.Moment
+    ) {}
+}
