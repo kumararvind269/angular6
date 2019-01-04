4f479be9c76e13c466a7f9f6f1978ff9e3dbd4e3
kumararvind269 <36441117+kumararvind269@users.noreply.github.com>
2019-01-04 07:21:07 +0530

    Add files via upload

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,1 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Filter } from '../shared/models/filter';
 
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css']
+})
+export class ContentComponent implements OnInit {
+public filterToList: Filter;
+public changedSlicerValue: number;
+
+  constructor() { }
+
+  ngOnInit() {}
+
+  public filterReceived(event: Filter): void {
+    this.filterToList = new Filter(
+      event.sourceCity,
+      event.destinationCity,
+      event.departureDate,
+      event.passengers,
+      event.returnDate
+    );
+  }
+
+  public slicerChangeReceiver(event: any): void {
+    this.changedSlicerValue = event;
+  }
+
+}
